267dd1c89e17ab7953d924963e3c5fdb701ec6b9
OUYANG-LIU-GITHUB <angelina.ouyangliu@gmail.com>
2025-05-07 09:20:54 +0800

    mobile bug fixed

--- js/application.js
+++ js/application.js
@@ -2,4 +2,16 @@
 window.requestAnimationFrame(function () {
   // 将游戏管理器实例保存到全局变量，这样HTMLActuator和其他组件可以访问
   window.gameManager = new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+
+  // 如果是移动设备，添加CSS类
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.body.classList.add('mobile-device');
+
+    // 阻止默认的iOS双指缩放
+    document.addEventListener('touchmove', function (e) {
+      if (e.touches.length > 1) {
+        e.preventDefault();
+      }
+    }, { passive: false });
+  }
 });
